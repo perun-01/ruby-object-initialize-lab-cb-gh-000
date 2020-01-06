5ecaea8d99c753a66e65d272c68d485083dd1422
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-06 00:09:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def initialize(dog_name)
+    @name = dog_name
+  end
+  def name
+    @name
+
+  end
+  def breed=(dog_name="Mutt")
+    @breed = dog_name
+  end
+  def breed
+    @breed
+
+  end
+end
